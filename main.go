0fa47148b55fce6830f3c285fd3d8d56b4666bb5
JonasS <jonass@dev.jsje.de>
2017-11-09 10:06:59 +0100

    Improved output - json instead of go-reflection - headers and body are only displayed on request (--headers and --body flags)

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"github.com/jonasprogrammer/docker-plugin-auth-debug/plugin"
 	"os/user"
 	"strconv"
+	"flag"
 )
 
 func main() {
 	p := plugin.NewLogAuthorizationPlugin()
+
+	flag.BoolVar(&p.Headers, "headers", false, "show headers")
+	flag.BoolVar(&p.Body, "body", false, "show request/response body")
+
+	flag.Parse()
+
 	h := authorization.NewHandler(p)
 	u, _ := user.Lookup("root")
 	gid, _ := strconv.Atoi(u.Gid)
--- plugin/log-authorization-plugin.go
+++ plugin/log-authorization-plugin.go
@@ -3,23 +3,56 @@ package plugin
 import (
 	"github.com/docker/go-plugins-helpers/authorization"
 	"fmt"
+	"encoding/json"
+	"os"
 )
 
 type LogAuthorizationPlugin struct {
+	Headers bool
+	Body    bool
 }
 
 func NewLogAuthorizationPlugin() *LogAuthorizationPlugin {
-	return &LogAuthorizationPlugin{}
+	return &LogAuthorizationPlugin{
+		Headers: false,
+		Body:    false,
+	}
 }
 
-func (*LogAuthorizationPlugin) AuthZReq(req authorization.Request) authorization.Response {
-	fmt.Println(req)
+func (p *LogAuthorizationPlugin) AuthZReq(req authorization.Request) authorization.Response {
+	if !p.Headers {
+		req.RequestHeaders = nil
+	}
+	if !p.Body {
+		req.RequestBody = nil
+	}
+
+	output, err := json.Marshal(req)
+	if err == nil {
+		fmt.Printf("request: %s\n", string(output))
+	} else {
+		fmt.Fprintf(os.Stderr, "request: marshalling error %v\n", err)
+	}
 
 	return authorization.Response{Allow: true}
 }
 
-func (*LogAuthorizationPlugin) AuthZRes(res authorization.Request) authorization.Response {
-	fmt.Println(res)
+func (p *LogAuthorizationPlugin) AuthZRes(res authorization.Request) authorization.Response {
+	if !p.Headers {
+		res.RequestHeaders = nil
+		res.ResponseHeaders = nil
+	}
+	if !p.Body {
+		res.RequestBody = nil
+		res.ResponseBody = nil
+	}
+
+	output, err := json.Marshal(res)
+	if err == nil {
+		fmt.Printf("response: %s\n", string(output))
+	} else {
+		fmt.Fprintf(os.Stderr, "response: marshalling error %v\n", err)
+	}
 
 	return authorization.Response{Allow: true}
 }
